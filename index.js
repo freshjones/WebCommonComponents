c87ec2c76a97d83324193ddd14412e3baf3caaf8
William Jones <billy@freshjones.com>
2018-01-02 11:48:34 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Sizes = require('./sizes');
+
+var commonElements = {
+  Sizes:Sizes
+}
+
+module.exports = commonElements;
---
+++ sizes.js
@@ -0,0 +1,9 @@
+const Sizes = {
+  xsmall: "480px",
+  small: "768px",
+  medium: "960px",
+  large: "1200px",
+  xlarge: "1400px",
+}
+
+module.exports = Sizes;
